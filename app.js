3d558f420a49d2dcbdc268b4b687fbd84502f5e9
Anshjatana <anshjatana1111@gmail.com>
2024-08-29 22:23:14 +0530

    Updated CORS Policy

--- app.js
+++ app.js
@@ -12,18 +12,19 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('Database connection error', err);
 });
 
-// middleware
+// CORS Configuration
+app.use(cors({
+    origin: 'https://melodyverse-ansh.netlify.app', // Ensure no trailing slash
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true // Allow cookies or other credentials
+  }));
+
+// middlewares
 app.use(express.json());
 app.use('/', require('./routes/auth'));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-//allow all requests from all origins & let the browser handle CORS
-app.use(cors({
-    origin: 'https://melodyverse-ansh.netlify.app/',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  }));
 
 
 const port = process.env.PORT || 8000;
